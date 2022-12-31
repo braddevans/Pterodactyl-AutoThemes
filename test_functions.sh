06ce249e13256967f2b44947d8402c32a363ee70
bradd evans <braddevans123@gmail.com>
2022-12-31 00:46:17 +0000

    push(bash rewrite): add version print to tests

--- test_functions.sh
+++ test_functions.sh
@@ -24,6 +24,7 @@ test() {
   echo -e "${GREEN}* ${YELLOW}os_ver: ${OS_VER}${GREEN}."
   echo -e "${GREEN}* ${YELLOW}os_ver_major: ${OS_VER_MAJOR}${GREEN}."
   echo -e "${GREEN}* ${YELLOW}ptero_install: ${PTERO_INSTALL}${GREEN}."
+  echo -e "${GREEN}* ${YELLOW}ptero_version: ${PANEL_VERSION}${GREEN}."
   echo -e "${GREEN}* ${YELLOW}ptero_dir: ${PTERO}${GREEN}."
   echo -e "${GREEN}* ${YELLOW}ptero_compatible:${IS_COMPATIBLE_VERSION}${GREEN}."
   echo -e "* Discord Support group: ${YELLOW}$(hyperlink "$SUPPORT_LINK")${RESET}"
