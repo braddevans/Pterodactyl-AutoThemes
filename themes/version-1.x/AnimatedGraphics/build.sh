c03ed6b76afdc573b55165290b4788a0bbf98e70
bradd evans <braddevans123@gmail.com>
2022-12-31 01:24:56 +0000

    push(bash rewrite): cleanup install.sh function calls

--- themes/version-1.x/AnimatedGraphics/build.sh
+++ themes/version-1.x/AnimatedGraphics/build.sh
@@ -13,163 +13,18 @@ set -e
 #
 ########################################################
 
-# Get the latest version before running the script #
-get_release() {
-  curl --silent \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/Ferks-FK/Pterodactyl-AutoThemes/releases/latest |
-    grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/'
-}
+MAINTAINER_REPO="braddevans"
 
-# Fixed Variables #
-SCRIPT_VERSION="$(get_release)"
-SUPPORT_LINK="https://discord.gg/buDBbSGJmQ"
+# import functions for the build.sh from github scripts/functions.sh
+curl -sSLo /tmp/ptero_functions.sh https://raw.githubusercontent.com/"${MAINTAINER_REPO}"/Pterodactyl-AutoThemes/main/scripts/functions.sh
+chmod +x /tmp/ptero_functions.sh
+source /tmp/ptero_functions.sh
 
 # Update Variables #
 update_variables() {
   BIGGER_CONSOLE="$PTERO/resources/scripts/components/server/StatGraphs.tsx"
   CONFIG_FILE="$PTERO/config/app.php"
   PANEL_VERSION="$(grep "'version'" "$CONFIG_FILE" | cut -c18-25 | sed "s/[',]//g")"
-}
-
-# Visual Functions #
-print_brake() {
-  for ((n = 0; n < $1; n++)); do
-    echo -n "#"
-  done
-  echo ""
-}
-
-print_warning() {
-  echo ""
-  echo -e "* ${YELLOW}WARNING${RESET}: $1"
-  echo ""
-}
-
-print_error() {
-  echo ""
-  echo -e "* ${RED}ERROR${RESET}: $1"
-  echo ""
-}
-
-print() {
-  echo ""
-  echo -e "* ${GREEN}$1${RESET}"
-  echo ""
-}
-
-hyperlink() {
-  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
-}
-
-GREEN="\e[0;92m"
-YELLOW="\033[1;33m"
-RED='\033[0;31m'
-RESET="\e[0m"
-
-# OS check #
-check_distro() {
-  if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$(echo "$ID" | awk '{print tolower($0)}')
-    OS_VER=$VERSION_ID
-  elif type lsb_release >/dev/null 2>&1; then
-    OS=$(lsb_release -si | awk '{print tolower($0)}')
-    OS_VER=$(lsb_release -sr)
-  elif [ -f /etc/lsb-release ]; then
-    . /etc/lsb-release
-    OS=$(echo "$DISTRIB_ID" | awk '{print tolower($0)}')
-    OS_VER=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then
-    OS="debian"
-    OS_VER=$(cat /etc/debian_version)
-  elif [ -f /etc/SuSe-release ]; then
-    OS="SuSE"
-    OS_VER="?"
-  elif [ -f /etc/redhat-release ]; then
-    OS="Red Hat/CentOS"
-    OS_VER="?"
-  else
-    OS=$(uname -s)
-    OS_VER=$(uname -r)
-  fi
-
-  OS=$(echo "$OS" | awk '{print tolower($0)}')
-  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
-}
-
-# Find where pterodactyl is installed #
-find_pterodactyl() {
-  print "Looking for your pterodactyl installation..."
-
-  sleep 2
-  if [ -d "/var/www/pterodactyl" ]; then
-    PTERO_INSTALL=true
-    PTERO="/var/www/pterodactyl"
-  elif [ -d "/var/www/panel" ]; then
-    PTERO_INSTALL=true
-    PTERO="/var/www/panel"
-  elif [ -d "/var/www/ptero" ]; then
-    PTERO_INSTALL=true
-    PTERO="/var/www/ptero"
-  else
-    PTERO_INSTALL=false
-  fi
-  # Update the variables after detection of the pterodactyl installation #
-  update_variables
-}
-
-# Verify Compatibility #
-compatibility() {
-  print "Checking if the addon is compatible with your panel..."
-
-  sleep 2
-  if [ "$PANEL_VERSION" == "1.11.2" ]; then
-    print "Compatible Version!"
-  else
-    print_error "Incompatible Version!"
-    exit 1
-  fi
-}
-
-# Install Dependencies #
-dependencies() {
-  print "Installing dependencies..."
-
-  if node -v &>/dev/null; then
-    print "The dependencies are already installed, skipping this step..."
-  else
-    case "$OS" in
-    debian | ubuntu)
-      curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs
-      ;;
-    centos)
-      [ "$OS_VER_MAJOR" == "7" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo yum install -y nodejs yarn
-      [ "$OS_VER_MAJOR" == "8" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo dnf install -y nodejs
-      ;;
-    esac
-  fi
-}
-
-# Panel Backup #
-backup() {
-  print "Performing security backup..."
-
-  if [ -d "$PTERO/PanelBackup[Auto-Themes]" ]; then
-    print "There is already a backup, skipping step..."
-  else
-    cd $PTERO
-    if [ -d "$PTERO/node_modules" ]; then
-      tar -czvf "PanelBackup[Auto-Themes].tar.gz" --exclude "node_modules" -- * .env
-      mkdir -p "$PTERO/PanelBackup[Auto-Themes]"
-      mv "$PTERO/PanelBackup[Auto-Themes].tar.gz" "$PTERO/PanelBackup[Auto-Themes]"
-    else
-      tar -czvf "PanelBackup[Auto-Themes].tar.gz" -- * .env
-      mkdir -p "$PTERO/PanelBackup[Auto-Themes]"
-      mv "$PTERO/PanelBackup[Auto-Themes].tar.gz" "$PTERO/PanelBackup[Auto-Themes]"
-    fi
-  fi
 }
 
 # Download Files #
